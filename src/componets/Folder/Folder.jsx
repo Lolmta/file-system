72197943f3dfd3314519e811d15cec8946c27b34
Lolita Popova <Lolita_Popova@epam.com>
2022-05-01 18:25:22 +0300

    2/toggle

--- src/componets/Folder/Folder.jsx
+++ src/componets/Folder/Folder.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Tree from './../Tree/Tree';
 import File from '../File/File';
-import { useState } from 'react';
-import styled from 'styled-components'
 
+import { useState } from 'react';
+
+import styled from 'styled-components'
 import { FaBeer } from "react-icons/fa";
 
 const Collapsible = styled.div`
@@ -30,7 +31,9 @@ const Folder = ({ data }) => {
 
   return (
     <div >
-      <FaBeer onClick={handleToggle}/> {data.name}
+      <div onClick={handleToggle}>
+        <FaBeer />{data.name}
+      </div>
       <Collapsible isOpen={isOpen}><Render /></Collapsible>
     </div>
   )
