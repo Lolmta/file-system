84be9cde0061911e975dea7362b9b10c234c45fc
Lolita Popova <Lolita_Popova@epam.com>
2022-05-03 12:19:32 +0300

    remove

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import './App.css';
 import Tree from './componets/Tree/Tree';
-import {setLastDeleted, setNewStructure} from './store/actions/actionCreators';
+import { setLastDeleted, setNewStructure } from './store/actions/actionCreators';
 import styled from 'styled-components';
 
 const Button = styled.button`
@@ -11,7 +11,9 @@ const Button = styled.button`
   border: 2px solid palevioletred;
   color: palevioletred;
   margin-left:20px;
+  margin-top:20px;
   padding: 0.25em 1em;
+  font-size:16px;
 `
 
 
@@ -21,7 +23,7 @@ const Button = styled.button`
 //     const newContent = [...content];
 
 //     if (path.length === 1) {
-        
+
 //         return newContent.filter(item => item.name !== path[0]);
 //     }
 
@@ -43,38 +45,35 @@ const Button = styled.button`
 const removeEntityByPath = (path, content, level) => {
     const newContent = [...content];
 
+    console.log(level+=1)
+
+
     if (path.length === 1) {
-        return newContent.filter(item =>  item.path.slice(1) !== path[0]
-        //     { 
-        //     console.log('item.path',item.path)
-        //     console.log('item.path.slice(1)/',item.path.split('/').slice(-1))
-            
-        //     console.log('path',path)
-        //     console.log('path[0]',path[0])
-
-        //     // const a = item.path.split('/');
-        //     // const b = a.slice(1, a.length);
-
-        // // console.log('a',a)
-        // // console.log('b',b)
      
-        //     return item.path !== path[0]
-        // }
         
-        );
+
+        return newContent.filter(el => el.path.split('/').slice(-1) != path[0])
+        // return newContent.filter(item =>{
+        //     debugger
+        //     const b = item.path.split('/').slice(-1)
+        //     console.log(b)
+        //     console.log('path',path[0])
+        //     return b[level] !== path[0]
+        // }
+
+       // );
     }
 
     const targetParent = path[0];
     const newPath = path.slice(1, path.length);
 
     for (const item of newContent) {
+       
         const a = item.path.split('/');
         const b = a.slice(1, a.length);
-        console.log('a',a)
-        console.log('b',b)
-
-        if (b[level] === targetParent) {
-            console.log('level',level)
+        
+        if (a[level] === targetParent) {
+   
             item.content = removeEntityByPath(newPath, item.content, level++);
         }
     }
@@ -94,7 +93,7 @@ const restoreEntityByPath = (path, content, lastDeleted) => {
     const targetParent = path[0];
     const newPath = path.slice(1, path.length);
     for (const item of newContent) {
-       
+
         if (item.name === targetParent) {
             item.content = restoreEntityByPath(newPath, item.content, lastDeleted);
         }
@@ -107,10 +106,10 @@ function App() {
     const dispatch = useDispatch();
     const structure = useSelector(state => state.fileSystem.structure);
     const lastDeleted = useSelector(state => state.fileSystem.lastDeleted);
-    
+
 
     const handleDelete = (item) => {
-        
+
         if (item.path === '/') {
             dispatch(setNewStructure({}));
             dispatch(setLastDeleted(item))
@@ -118,8 +117,8 @@ function App() {
         }
 
         const path = item.path.split('/');
-    
-        const newStructure = {...structure};
+
+        const newStructure = { ...structure };
         newStructure.content = removeEntityByPath(path.slice(1, path.length), newStructure.content, 0); // version for remove by Path and path
         //newStructure.content = removeEntityByPath(path.slice(1, path.length), newStructure.content); // version for remove by Path and name
         dispatch(setNewStructure(newStructure));
@@ -128,15 +127,15 @@ function App() {
 
     const handleRestore = () => {
 
-        if(lastDeleted.path === '/'){
-        dispatch(setNewStructure(lastDeleted))
-        return;
-    }
+        if (lastDeleted.path === '/') {
+            dispatch(setNewStructure(lastDeleted))
+            return;
+        }
 
         const path = lastDeleted.path.split('/');
 
-    
-        const newStructure = {...structure};
+
+        const newStructure = { ...structure };
         //newStructure.content = restoreEntityByPath(path.slice(1, path.length), newStructure.content, 0, lastDeleted); 
         newStructure.content = restoreEntityByPath(path.slice(1, path.length), newStructure.content, lastDeleted);
         dispatch(setNewStructure(newStructure));
@@ -148,7 +147,7 @@ function App() {
 
     return (
         <div className="App">
-            <Tree data={structure} onDelete={handleDelete}/>
+            <Tree data={structure} onDelete={handleDelete} />
             <Button onClick={handleRestore}>Restore last deleted</Button>
         </div>
     );
