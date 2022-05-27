bad4b78572f4c1ed7d0f3fd9cbf31a2ca79f5065
Lolita Popova <Lolita_Popova@epam.com>
2022-05-27 16:25:44 +0300

    test v7

--- src/componets/File/File.test.js
+++ src/componets/File/File.test.js
@@ -1,45 +1,40 @@
-import { render, screen, fireEvent, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import File from './File';
 
 const data = {
     name: 'App.jsx'
 }
 
-afterEach(cleanup)
-
 describe('File filling', () => {
+
     test('name in document', () => {
         render(<File data={data} />);
-        expect(screen.getByText(data.name)).toBeInTheDocument()
+        const fileName = screen.getByText(data.name);
+        expect(fileName).toBeInTheDocument()
     })
 
     test('file icon in document', () => {
-        const { getByTestId } = render(<File data={data} />);
-        expect(getByTestId("BsFileEarmark")).toBeInTheDocument();
+        render(<File data={data} />);
+        const fileIcon = screen.getByTestId("BsFileEarmark")
+        expect(fileIcon).toBeInTheDocument()
     })
 
     test('button delete in document', () => {
-        const { getByTestId } = render(<File data={data} />);
-        expect(getByTestId("onDelete")).toBeInTheDocument();
+       render(<File data={data} />);
+       const deleteButton = screen.getByTestId("onDelete")
+        expect(deleteButton).toBeInTheDocument();
     })
 })
 
 describe('File button', () => {
-    test('name in document', () => {
-        render(<File data={data} />);
-        expect(screen.getByText(data.name)).toBeInTheDocument()
+     test('button click', () => {
+        const handleClick = jest.fn()
+        render(<File data={data} onDelete={handleClick}  />);
+        const deleteButton = screen.getByTestId("onDelete")
+        expect(handleClick).not.toBeCalled()
+        fireEvent.click(deleteButton)
+        expect(handleClick).toHaveBeenCalledTimes(1)
     })
-
-    //  test('name in document', async () => {
-    //     const handleClick = jest.fn()
-
-    //      const { getByTestId } =  render(<File data={data} onDelete={handleClick} />);
-        
-    //     await fireEvent.click(screen.getByTestId("onDelete"))
-    //     // expect(handleClick).toHaveBeenCalledTimes(1)
-
-    // })
-
 })
 
 
