4c6031db48fdbbd216e943fd6793d7445038e5a7
Lolita Popova <Lolita_Popova@epam.com>
2022-05-28 22:47:25 +0300

    test v9

--- src/componets/File/File.test.js
+++ src/componets/File/File.test.js
@@ -1,41 +1,36 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import File from './File';
+import { render, screen, fireEvent } from "@testing-library/react";
+import File from "./File";
 
-const data = {
-    name: 'App.jsx'
-}
+const data = {name: "App.jsx"};
 
-describe('File filling', () => {
+describe("File filling", () => {
+  beforeEach(() => {
+    render(<File data={data} />);
+  });
 
-    test('name in document', () => {
-        render(<File data={data} />);
-        const fileName = screen.getByText(data.name);
-        expect(fileName).toBeInTheDocument()
-    })
+  test("name in document", () => {
+    const fileName = screen.getByText(data.name);
+    expect(fileName).toBeInTheDocument();
+  });
 
-    test('file icon in document', () => {
-        render(<File data={data} />);
-        const fileIcon = screen.getByTestId("BsFileEarmark")
-        expect(fileIcon).toBeInTheDocument()
-    })
+  test("file icon in document", () => {
+    const fileIcon = screen.getByTestId("BsFileEarmark");
+    expect(fileIcon).toBeInTheDocument();
+  });
 
-    test('button delete in document', () => {
-       render(<File data={data} />);
-       const deleteButton = screen.getByTestId("onDelete")
-        expect(deleteButton).toBeInTheDocument();
-    })
-})
+  test("button delete in document", () => {
+    const deleteButton = screen.getByTestId("onDelete");
+    expect(deleteButton).toBeInTheDocument();
+  });
+});
 
-describe('File button', () => {
-     test('button click', () => {
-        const handleClick = jest.fn()
-        render(<File data={data} onDelete={handleClick}  />);
-        const deleteButton = screen.getByTestId("onDelete")
-        expect(handleClick).not.toBeCalled()
-        fireEvent.click(deleteButton)
-        expect(handleClick).toHaveBeenCalledTimes(1)
-    })
-})
-
-
-
+describe("File button", () => {
+  test("button click", () => {
+    const handleClick = jest.fn();
+    render(<File data={data} onDelete={handleClick} />);
+    const deleteButton = screen.getByTestId("onDelete");
+    expect(handleClick).not.toBeCalled();
+    fireEvent.click(deleteButton);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
--- src/componets/Folder/Folder.test.js
+++ src/componets/Folder/Folder.test.js
@@ -1,54 +1,49 @@
 import Folder from "./Folder";
-import { render, screen, fireEvent  } from '@testing-library/react';
+import { render, screen, fireEvent } from "@testing-library/react";
 
-describe('Folder filling', () => {
-    const data = {
-        name: 'App.jsx'
-    }
-    test('name in document', () => {
-        render(<Folder data={data} />);
-        const fileName = screen.getByText(data.name);
-        expect(fileName).toBeInTheDocument()
-    })
+const data = { name: "App.jsx" };
 
-    test('button delete in document', () => {
-       render(<Folder data={data} />);
-       const deleteButton = screen.getByTestId("onDelete")
-        expect(deleteButton).toBeInTheDocument();
-    })
+describe("Folder filling", () => {
+  beforeEach(() => {
+    render(<Folder data={data} />);
+  });
+  test("name in document", () => {
+    const fileName = screen.getByText(data.name);
+    expect(fileName).toBeInTheDocument();
+  });
 
-    test('button delete in document', () => {
-        render(<Folder data={data} />);
-        const deleteButton = screen.getByTestId('toggle-button')
-         expect(deleteButton).toBeInTheDocument();
-     })
+  test("button delete in document", () => {
+    const deleteButton = screen.getByTestId("onDelete");
+    expect(deleteButton).toBeInTheDocument();
+  });
 
-})
+  test("button delete in document", () => {
+    const deleteButton = screen.getByTestId("toggle-button");
+    expect(deleteButton).toBeInTheDocument();
+  });
+});
 
-describe('Folder button', () => {
-    const data = {
-        name: 'App.jsx'
-    }
-    test('button click', () => {
-        const handleClick = jest.fn()
-        render(<Folder data={data} onDelete={handleClick}  />);
-        const deleteButton = screen.getByTestId("onDelete")
-        expect(handleClick).not.toBeCalled()
-        fireEvent.click(deleteButton)
-        expect(handleClick).toHaveBeenCalledTimes(1)
-    })
+describe("Folder button", () => {
+  test("button click", () => {
+    const handleClick = jest.fn();
+    render(<Folder data={data} onDelete={handleClick} />);
+    const deleteButton = screen.getByTestId("onDelete");
+    expect(handleClick).not.toBeCalled();
+    fireEvent.click(deleteButton);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
 
-    test('toggle button click', async () => {
-        render(<Folder data={data} />);
-        const toggleButton = screen.getByTestId('toggle-button')
-        let folderIcon 
-        folderIcon = await screen.getByTestId('folder-close')
-        expect(folderIcon).toBeInTheDocument()
-        fireEvent.click(toggleButton)
-        folderIcon = await screen.getByTestId('folder-open')
-        expect( folderIcon).toBeInTheDocument()
-        fireEvent.click(toggleButton)
-        folderIcon = await screen.getByTestId('folder-close')
-        expect( folderIcon).toBeInTheDocument()
-    })
-})
+  test("toggle button click", async () => {
+    render(<Folder data={data} />);
+    const toggleButton = screen.getByTestId("toggle-button");
+    let folderIcon;
+    folderIcon = await screen.findByTestId("folder-close");
+    expect(folderIcon).toBeInTheDocument();
+    fireEvent.click(toggleButton);
+    folderIcon = await screen.findByTestId("folder-open");
+    expect(folderIcon).toBeInTheDocument();
+    fireEvent.click(toggleButton);
+    folderIcon = await screen.findByTestId("folder-close");
+    expect(folderIcon).toBeInTheDocument();
+  });
+});
