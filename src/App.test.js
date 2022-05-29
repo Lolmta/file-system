7071ed3042506ab8eda4517f18c2dd88c59f8c2d
Lolita Popova <Lolita_Popova@epam.com>
2022-05-29 21:21:03 +0300

    test v11

--- src/App.test.js
+++ src/App.test.js
@@ -3,65 +3,67 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
-import { setLastDeleted } from './store/actions/actionCreators';
+import { setLastDeleted } from "./store/actions/actionCreators";
 
 
-beforeEach(() => {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
-});
 
 describe("Restore btn", () => {
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+  });
+  
+  let deleteButton;
 
-  let deleteButton
-
-  test('button', () => {
-    deleteButton = screen.getByRole('button');
+  test("button", () => {
+    deleteButton = screen.getByRole("button");
     expect(deleteButton).toBeInTheDocument();
-    expect(deleteButton.textContent).toBe('Restore last deleted')
+    expect(deleteButton.textContent).toBe("Restore last deleted");
   });
 
-  test('button sh be disabled', () => {
-    deleteButton = screen.getByRole('button');
+  test("button sh be disabled", () => {
+    deleteButton = screen.getByRole("button");
     expect(deleteButton).toBeDisabled();
   });
 
-  test('button sh not be disabled', async() => {
-    store.dispatch(setLastDeleted({name:'App.js'}))
-    deleteButton = await screen.findByRole('button');
+  test("button sh not be disabled", async () => {
+    store.dispatch(setLastDeleted({ name: "App.js" }));
+    deleteButton = await screen.findByRole("button");
     expect(deleteButton).not.toBeDisabled();
   });
 
-  test('button sh not be disabled', async() => {
-    deleteButton = await screen.findByRole('button');
+  test("button sh not be disabled", async () => {
+    deleteButton = await screen.findByRole("button");
     const handleClick = jest.fn();
-    let lastDel =  {
+    let lastDel = {
       name: "App.js",
       path: "/src/App.js",
       type: "file",
-      content: null
-    }
-    await store.dispatch(setLastDeleted(lastDel))
-    deleteButton.addEventListener("click",handleClick)
-    fireEvent.click( deleteButton)
+      content: null,
+    };
+    await store.dispatch(setLastDeleted(lastDel));
+    deleteButton.addEventListener("click", handleClick);
+    fireEvent.click(deleteButton);
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
 
-  test('button sh not be disabled', async() => {
-    deleteButton = await screen.findByRole('button');
+  test("button sh not be disabled", async () => {
+    deleteButton = await screen.findByRole("button");
     const handleClick = jest.fn();
-    let lastDel =  {
+    let lastDel = {
       name: "App.js",
       path: "/",
       type: "file",
-      content: null
-    }
-    await store.dispatch(setLastDeleted(lastDel))
-    deleteButton.addEventListener("click",handleClick)
-    fireEvent.click( deleteButton)
+      content: null,
+    };
+    await store.dispatch(setLastDeleted(lastDel));
+    deleteButton.addEventListener("click", handleClick);
+    fireEvent.click(deleteButton);
     expect(handleClick).toHaveBeenCalledTimes(1);
   });
 });
+
+
