37b23fcba938adc6549d607772e88b5b4a9f47a9
Lolita Popova <Lolita_Popova@epam.com>
2022-05-30 00:58:44 +0300

    app tests restore act

--- src/_tests_/AppRestore.test.js
+++ src/_tests_/AppRestore.test.js
@@ -9,60 +9,74 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import { act } from "@testing-library/react";
 
 describe("Restore btn test", () => {
-    beforeEach(() => {
+  beforeEach(() => {
+    act(() => {
       render(
         <Provider store={store}>
           <App />
         </Provider>
       );
     });
-  
-    let deleteButton;
-  
-    test("should render the restore btn", () => {
-      deleteButton = screen.getByRole("button");
-      expect(deleteButton).toBeInTheDocument();
-      expect(deleteButton.textContent).toBe("Restore last deleted");
-    });
-  
-    test("should be disabled", () => {
-      deleteButton = screen.getByRole("button");
-      expect(deleteButton).toBeDisabled();
-    });
-  
-    test("should not be disabled", async () => {
-      store.dispatch(setLastDeleted({ name: "App.js" }));
-      deleteButton = await screen.findByRole("button");
-      expect(deleteButton).not.toBeDisabled();
-    });
-  
-    test("should work on click if path > 1", async () => {
-      deleteButton = await screen.findByRole("button");
-      const handleClick = jest.fn();
-      let lastDel = {
-        name: "App.js",
-        path: "/src/App.js",
-        type: "file",
-        content: null,
-      };
-      await store.dispatch(setLastDeleted(lastDel));
-      deleteButton.addEventListener("click", handleClick);
-      fireEvent.click(deleteButton);
-      expect(handleClick).toHaveBeenCalledTimes(1);
-    });
-  
-    test("should work on click if path == 1", async () => {
-      deleteButton = await screen.findByRole("button");
-      const handleClick = jest.fn();
-      let lastDel = {
-        name: "App.js",
-        path: "/",
-        type: "file",
-        content: null,
-      };
-      await store.dispatch(setLastDeleted(lastDel));
-      deleteButton.addEventListener("click", handleClick);
-      fireEvent.click(deleteButton);
-      expect(handleClick).toHaveBeenCalledTimes(1);
-    });
   });
+
+  let deleteButton;
+
+  test("should render the restore btn", () => {
+    deleteButton = screen.getByRole("button");
+    expect(deleteButton).toBeInTheDocument();
+    expect(deleteButton.textContent).toBe("Restore last deleted");
+  });
+
+  test("should be disabled", () => {
+    deleteButton = screen.getByRole("button");
+    expect(deleteButton).toBeDisabled();
+  });
+
+  test("should not be disabled", async () => {
+    act(() => {
+      store.dispatch(setLastDeleted({ name: "App.js" }));
+    });
+    deleteButton = await screen.findByRole("button");
+    expect(deleteButton).not.toBeDisabled();
+  });
+
+  test("should work on click if path > 1", async () => {
+    deleteButton = await screen.findByRole("button");
+    const handleClick = jest.fn();
+    let lastDel = {
+      name: "App.js",
+      path: "/src/App.js",
+      type: "file",
+      content: null,
+    };
+
+    act(() => {
+      store.dispatch(setLastDeleted(lastDel));
+    });
+    deleteButton.addEventListener("click", handleClick);
+    act(() => {
+      fireEvent.click(deleteButton);
+    });
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+
+  test("should work on click if path == 1", async () => {
+    deleteButton = await screen.findByRole("button");
+    const handleClick = jest.fn();
+    let lastDel = {
+      name: "App.js",
+      path: "/",
+      type: "file",
+      content: null,
+    };
+    act(() => {
+      store.dispatch(setLastDeleted(lastDel));
+    });
+    deleteButton.addEventListener("click", handleClick);
+
+    act(() => {
+      fireEvent.click(deleteButton);
+    });
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+});
