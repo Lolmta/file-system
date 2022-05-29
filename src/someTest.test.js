68eeaf1ce877b5a910d0e1752c9e5e452b6e7bda
Lolita Popova <Lolita_Popova@epam.com>
2022-05-29 22:12:43 +0300

    test v12

---
+++ src/someTest.test.js
@@ -0,0 +1,31 @@
+import Tree from "./componets/Tree/Tree";
+// import { act } from 'react-dom/test-utils';
+import { act } from "@testing-library/react";
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./store/index";
+
+test('', () => {
+    const handleClick = jest.fn();
+  act(() => {
+    render(
+      <Provider store={store}>
+        <App>
+            <Tree onDelete={handleClick}/>
+        </App>
+      </Provider>
+    )
+  })
+
+  let b = screen.getAllByTestId('onDelete')
+  act(()=>{
+    b[0].addEventListener('click', handleClick)
+      fireEvent.click(b[0])
+  })
+
+  expect(handleClick).toBeCalled()
+
+ 
+})
