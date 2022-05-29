61d47ebdfecd140d8e598378e73c84291b49db6a
Lolita Popova <Lolita_Popova@epam.com>
2022-05-29 22:13:27 +0300

    test v12

--- src/someTest.test.js
+++ src/someTest.test.js
@@ -1,5 +1,4 @@
 import Tree from "./componets/Tree/Tree";
-// import { act } from 'react-dom/test-utils';
 import { act } from "@testing-library/react";
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
@@ -7,25 +6,23 @@ import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store/index";
 
-test('', () => {
-    const handleClick = jest.fn();
+test("", () => {
+  const handleClick = jest.fn();
   act(() => {
     render(
       <Provider store={store}>
         <App>
-            <Tree onDelete={handleClick}/>
+          <Tree onDelete={handleClick} />
         </App>
       </Provider>
-    )
-  })
+    );
+  });
 
-  let b = screen.getAllByTestId('onDelete')
-  act(()=>{
-    b[0].addEventListener('click', handleClick)
-      fireEvent.click(b[0])
-  })
+  let b = screen.getAllByTestId("onDelete");
+  act(() => {
+    b[0].addEventListener("click", handleClick);
+    fireEvent.click(b[0]);
+  });
 
-  expect(handleClick).toBeCalled()
-
- 
-})
+  expect(handleClick).toBeCalled();
+});
