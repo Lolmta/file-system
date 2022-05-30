83fbbde3a8d45e09b3172fa5a8106e0cab3a3996
Lolita Popova <Lolita_Popova@epam.com>
2022-05-30 15:51:03 +0300

    tree test 2v

--- src/componets/Tree/Tree.test.js
+++ src/componets/Tree/Tree.test.js
@@ -1,31 +1,62 @@
 import Tree from './Tree.jsx';
 import { render, screen } from '@testing-library/react';
 
+
 describe('Tree test', () => {
-  test('should notify that everything is removed', () => {
-    const data = {};
-    render(<Tree data={data} />);
+  test('should render by data', () => {
+    let data = {};
+    const {rerender} = render(<Tree data={data} />)
+
     expect(
       screen.getByText(/everything has been removed/i)
     ).toBeInTheDocument();
-  });
-
-  test('should render the file', () => {
-    const data = {
+ 
+     data = {
       type: 'file',
-      path: '/src/App.js',
+      path: '/src/App.css',
     };
-    render(<Tree key={data.path} data={data} />);
+    rerender(<Tree data={data} />);
     expect(screen.getByTestId('file'));
-  });
-
-  test('should render folder', () => {
-    const data = {
+ 
+     data = {
       type: 'folder',
-      path: '/src/App.js',
-      content: [{ name: 'App.js' }],
+      path: '/src/store',
+      content: [{ name: 'actionCreators.js',
+      path: '/src/store/actionCreators.js' }],
     };
-    render(<Tree key={data.path} data={data} />);
+    rerender(<Tree data={data} />);
     expect(screen.getByTestId('StyledFile'));
+
   });
 });
+
+
+
+// describe('Tree test', () => {
+//   test('should notify that everything is removed', () => {
+//     const data = {};
+//     render(<Tree data={data} />);
+//     expect(
+//       screen.getByText(/everything has been removed/i)
+//     ).toBeInTheDocument();
+//   });
+
+//   test('should render the file', () => {
+//     const data = {
+//       type: 'file',
+//       path: '/src/App.js',
+//     };
+//     render(<Tree data={data} />);
+//     expect(screen.getByTestId('file'));
+//   });
+
+//   test('should render folder', () => {
+//     const data = {
+//       type: 'folder',
+//       path: '/src/App.js',
+//       content: [{ name: 'App.js' }],
+//     };
+//     render(<Tree data={data} />);
+//     expect(screen.getByTestId('StyledFile'));
+//   });
+// });
